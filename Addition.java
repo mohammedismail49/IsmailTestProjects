72546195f66a85eef132137084d58431149d8e5e
mohammedismail49 <112697953+mohammedismail49@users.noreply.github.com>
2023-01-18 16:17:08 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,18 @@
+package testingExample.addTest;
+
+public class Addition {
+	
+public int sum(int var1 , int var2) {
+	
+	System.out.println("Adding values: "+var1 + " + "+var2);
+	
+	return var1+var2;
+}
+
+public int substract(int var1 , int var2) {
+	System.out.println("substracting values: "+var1+" - "+var2);
+	return var1-var2;
+	
+}
+
+}
---
+++ App.java
@@ -0,0 +1,13 @@
+package testingExample.addTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
